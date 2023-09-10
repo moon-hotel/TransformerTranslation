3ef5276470f49125c58a945c297a13b9ffadd112
nulls8 <moon-hotel@hotmail.com>
2023-09-10 14:17:11 +0800

    调整示例代码

--- test/test_MyTransformer.py
+++ test/test_MyTransformer.py
@@ -2,6 +2,7 @@ import sys
 
 sys.path.append('../')
 from model.MyTransformer import MyTransformer
+from model.MyTransformer import MyMultiheadAttention
 import torch
 
 if __name__ == '__main__':
@@ -11,16 +12,21 @@ if __name__ == '__main__':
     tgt_len = 6
     num_head = 8
     src = torch.rand((src_len, batch_size, dmodel))  # shape: [src_len, batch_size, embed_dim]
-    src_key_padding_mask = torch.tensor([[True, True, True, False, False],
-                                         [True, True, True, True, False]])  # shape: [batch_size, src_len]
+    src_key_padding_mask = torch.tensor([[False, False, False, True, True],
+                                         [False, False, False, False, True]])  # shape: [batch_size, src_len]
 
     tgt = torch.rand((tgt_len, batch_size, dmodel))  # shape: [tgt_len, batch_size, embed_dim]
-    tgt_key_padding_mask = torch.tensor([[True, True, True, False, False, False],
-                                         [True, True, True, True, False, False]])  # shape: [batch_size, tgt_len]
+    tgt_key_padding_mask = torch.tensor([[False, False, False, True, True, True],
+                                         [False, False, False, False, True, True]])  # shape: [batch_size, tgt_len]
 
     #   ============ 测试 MyMultiheadAttention ============
-    # my_mh = MyMultiheadAttention(embed_dim=dmodel, num_heads=num_head)
-    # r = my_mh(src, src, src, key_padding_mask=src_key_padding_mask)
+    my_mh = MyMultiheadAttention(embed_dim=dmodel, num_heads=num_head)
+    r = my_mh(src, src, src, key_padding_mask=src_key_padding_mask)
+    print(r[0].shape)
+
+    mh = torch.nn.MultiheadAttention(embed_dim=dmodel, num_heads=num_head)
+    r = mh(src, src, src, key_padding_mask=src_key_padding_mask)
+    print(r[0].shape)
 
     #  ============ 测试 MyTransformerEncoderLayer ============
     # my_transformer_encoder_layer = MyTransformerEncoderLayer(d_model=dmodel, nhead=num_head)
@@ -44,12 +50,12 @@ if __name__ == '__main__':
     # print(out.shape)
 
     # ============ 测试 MyTransformer ============
-    my_transformer = MyTransformer(d_model=dmodel, nhead=num_head, num_encoder_layers=6,
-                                   num_decoder_layers=6, dim_feedforward=500)
-    src_mask = my_transformer.generate_square_subsequent_mask(src_len)
-    tgt_mask = my_transformer.generate_square_subsequent_mask(tgt_len)
-    out = my_transformer(src=src, tgt=tgt, tgt_mask=tgt_mask,
-                         src_key_padding_mask=src_key_padding_mask,
-                         tgt_key_padding_mask=tgt_key_padding_mask,
-                         memory_key_padding_mask=src_key_padding_mask)
-    print(out.shape)
+    # my_transformer = MyTransformer(d_model=dmodel, nhead=num_head, num_encoder_layers=6,
+    #                                num_decoder_layers=6, dim_feedforward=500)
+    # src_mask = my_transformer.generate_square_subsequent_mask(src_len)
+    # tgt_mask = my_transformer.generate_square_subsequent_mask(tgt_len)
+    # out = my_transformer(src=src, tgt=tgt, tgt_mask=tgt_mask,
+    #                      src_key_padding_mask=src_key_padding_mask,
+    #                      tgt_key_padding_mask=tgt_key_padding_mask,
+    #                      memory_key_padding_mask=src_key_padding_mask)
+    # print(out.shape)
